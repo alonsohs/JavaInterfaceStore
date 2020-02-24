a7b614f7089098e7d0d13b3dcdaf27d3ed53d294
Alonso <alonsohdez14@gmail.com>
2020-02-23 18:33:07 -0600

    Final errors correction

--- MainInterface.java
+++ MainInterface.java
@@ -1,17 +1,27 @@
 import javax.swing.*;
 import java.awt.*;
 
+/* Interfaz grafica creada para la clase de Programación II
+*  Hecha por: -Alonso Hernández Sastré
+*             -Omar Valencia Diaz
+*             -Ximena Chacón Rojas
+*             -Valeria Luna Galicia
+*             -Efren Montiel Torres
+* Fecha:  23-02-2020
+* */
+
 public class MainInterface extends JFrame {
     public MainInterface(){
+        super("Interfaz gráfica");
         PanelProducto producto1 = new PanelProducto("iPhone 11", "Imagenes/iphone11.png", "17, 000");
         PanelProducto producto2 = new PanelProducto("Huawei P30", "Imagenes/huaweip30.png", "24,000");
         PanelProducto producto3 = new PanelProducto("Samsung S20", "Imagenes/samsungs20.png", "22,000" );
         PanelBuscador buscador = new PanelBuscador();
-        JPanel panelResultados = new JPanel();
-        panelResultados.setLayout(new GridLayout(1,3));
-        panelResultados.add(producto1, BorderLayout.CENTER);
-        panelResultados.add(producto2, BorderLayout.CENTER);
-        panelResultados.add(producto3, BorderLayout.CENTER);
+        JPanel panelProductos = new JPanel();
+        panelProductos.setLayout(new GridLayout(1,3));
+        panelProductos.add(producto1, BorderLayout.CENTER);
+        panelProductos.add(producto2, BorderLayout.CENTER);
+        panelProductos.add(producto3, BorderLayout.CENTER);
 
         JPanel panelBuscador = new JPanel();
         panelBuscador.setLayout(new GridLayout(1,3));
@@ -27,7 +37,7 @@ public class MainInterface extends JFrame {
         JPanel marcoFinal = new JPanel();
         marcoFinal.setLayout(new BoxLayout(marcoFinal, BoxLayout.Y_AXIS));
         marcoFinal.add(panelBuscador);
-        marcoFinal.add(panelResultados);
+        marcoFinal.add(panelProductos);
         Dimension dimension = new Dimension(1200,800) ;
         marcoFinal.setPreferredSize(dimension);
         JScrollPane scroll = new JScrollPane(marcoFinal);
--- PanelBuscador.java
+++ PanelBuscador.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class PanelBuscador extends JPanel {
-        JLabel empresa;
+      
         JTextField textoBuscador = new JTextField();
 
     public PanelBuscador(){
